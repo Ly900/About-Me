f9faa0ec94c2795d0a1ec881810868c1f566b8d8
Ly Nguyen <Ly900@aol.com>
2016-01-07 13:56:13 -0500

    effort to fix nav toggle issues

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 
-  $("ul#nav-links").hide();
 
-  $("li#menu-icon").on("click", function(){
-    $("ul#nav-links").slideToggle("fast", function(){
-      $("section.image").toggleClass("moved");
-    });
-  });
+  // console.log($(window).width());
+  //
+  // var $window = $(window);
+  // if ($window.width() > 339){
+  //   alert("hi!");
+  // };
+
+  // $("ul#nav-links").hide();
+
+  $("li#menu-icon").on("click", navToggle);
+
+  function moveMainImage(){
+    $("section.image").toggleClass("moved");
+  };
+
+  function navToggle(){
+    $("ul#nav-links").slideToggle("fast", moveMainImage);
+  };
 
 })
