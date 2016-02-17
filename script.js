2e6db862be21ab771eb60d001d1a28c264779eb0
Ly Nguyen <Ly900@aol.com>
2016-02-16 20:02:42 -0500

    refactor js code using revealing pattern module

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
 $(document).ready(function(){
 
-  var removeScoot = function() {
-    $("div.scoot").removeClass("scootDown");
-  }
+  var aboutMePage = (function() {
 
-  var navLinkClick = function() {
-    $("div#nav-links a").on("click", function(event) {
-      $('#toggle').attr('checked', false);
-      removeScoot();
-    });
-  }
-
-  var toggleNav = function() {
-    var scrollTop = $(window).scrollTop();
-    var inputChecked = $("input#toggle").is(":checked");
-    var $divScoot = $("div.scoot");
-    if (scrollTop < 20 & inputChecked) {
-      $divScoot.addClass("scootDown");
-    } else if (scrollTop < 20 & !inputChecked) {
-      $divScoot.removeClass("scootDown");
-    } else if (scrollTop > 20 & !inputChecked) {
-      $divScoot.removeClass("scootDown");
+    function removeScoot() {
+      $("div.scoot").removeClass("scootDown");
     };
-    navLinkClick();
-  }; // ends toggleNav function
 
-  $("input#toggle").on("click", toggleNav);
+    function navLinkClick() {
+      $("div#nav-links a").on("click", function(event) {
+        $('#toggle').attr('checked', false);
+        removeScoot();
+      });
+    };
 
-  var makeSkillBubbles = {
-    skills:
-    [
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Bootstrap",
-    "Javascript",
-    "JQuery",
-    "Ajax",
-    "Angular",
-    "Git",
-    "Github",
-    "Ruby",
-    "Rails",
-    "SQL",
-    "MongoDB",
-    "PostgresQL",
-    "Node.js",
-    "Express"
-    ],
-    makeBubbles: function() {
-      for (var i = 0; i < makeSkillBubbles.skills.length; i++) {
-        var skill = "<li>" + this.skills[i] + "</li>";
-        $("ul.skills").append(skill);
-        console.log("Hi! Thanks for visiting!");
+    function toggleNav() {
+      var scrollTop = $(window).scrollTop();
+      var inputChecked = $("input#toggle").is(":checked");
+      var $divScoot = $("div.scoot");
+      if (scrollTop < 20 & inputChecked) {
+        $divScoot.addClass("scootDown");
+      } else if (scrollTop < 20 & !inputChecked) {
+        $divScoot.removeClass("scootDown");
+      } else if (scrollTop > 20 & !inputChecked) {
+        $divScoot.removeClass("scootDown");
+      };
+      navLinkClick();
+    }; // ends toggleNav function
+
+    $("input#toggle").on("click", toggleNav);
+
+    var makeSkillBubbles = {
+      skills:
+      [
+      "HTML5",
+      "CSS3",
+      "SASS",
+      "Bootstrap",
+      "Javascript",
+      "JQuery",
+      "Ajax",
+      "Angular",
+      "Git",
+      "Github",
+      "Ruby",
+      "Rails",
+      "SQL",
+      "MongoDB",
+      "PostgresQL",
+      "Node.js",
+      "Express",
+      "Photoshop CC"
+      ],
+      makeBubbles: function() {
+        for (var i = 0; i < makeSkillBubbles.skills.length; i++) {
+          var skill = "<li>" + this.skills[i] + "</li>";
+          $("ul.skills").append(skill);
+        }
       }
     }
-  }
-  makeSkillBubbles.makeBubbles();
 
+    makeSkillBubbles.makeBubbles();
 
+})();
 
 }); // ends document.ready
